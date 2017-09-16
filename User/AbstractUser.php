db86f380c8ed5cd9ccbd872693694b2fdd634235
Alexandre Waeles <contact@axelandre42.net>
2017-09-16 14:43:08 +0200

    Added a lot of comments !

--- User/AbstractUser.php
+++ User/AbstractUser.php
@@ -2,6 +2,7 @@
 namespace Axelandre42\User\CoreBundle\User;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
@@ -46,37 +47,82 @@ abstract class AbstractUser implements UserInterface, \Serializable
     public function __construct()
     {
         $this->isActive = true;
+		$this->groups = new ArrayCollection();
     }
 
+	// Username, getter.
     public function getUsername()
     {
         return $this->username;
     }
 
+	
+	// Password, getter.
     public function getPassword()
     {
         return $this->password;
     }
 
-    public function getRoles()
+	// User-specific roles, getter.
+	public function getUserRoles()
     {
-        return $this->roles;
+		return $this->roles;
     }
 
+	
+	// All roles inherited from groups, getter.
+	public function getGroupsRoles()
+    {
+		$array = $this->groups->toArray();
+		$roles = array();
+		
+		foreach ($array as $value)
+		{
+			$roles = array_merge($roles, $value->getRoles());
+		}
+		
+		return $roles;
+    }
+
+	// All roles including groups roles and user roles, getter.
+    public function getRoles()
+    {
+		return array_merge($this->getUserRoles(), $this->getGroupsRoles());
+    }
+
+	// Adds user-specific role, pseudo-setter.
+	public function addRole($role)
+	{
+		array_push($this->roles, $role);
+	}
+
+	// Removes user-specific role, pseudo-setter.
+	public function removeRole($role)
+	{
+		if(($key = array_search($role, $this->roles)) !== false) {
+			unset($this->roles[$key]);
+		}
+	}
+
+	// Groups, getter.
 	public function getGroups()
     {
         return $this->groups;
     }
-	
+
+	// Salt (unused), getter.
 	public function getSalt()
 	{
 		return null;
 	}
 
+	// Erase password.
     public function eraseCredentials()
     {
+		$this->password = "";
     }
 
+	// Serialize.
     public function serialize()
     {
         return serialize(array(
@@ -87,6 +133,7 @@ abstract class AbstractUser implements UserInterface, \Serializable
         ));
     }
 
+	// Unserialize.
     public function unserialize($serialized)
     {
         list (
--- User/Group.php
+++ User/Group.php
@@ -22,6 +22,11 @@ class Group
 	private $roles;
 	
 	/**
+	 * @ORM\Column(type="string", length=64)
+	 */
+	private $name;
+	
+	/**
      * @ManyToMany(targetEntity="Group")
      * @JoinTable(name="permissions",
      *      joinColumns={@JoinColumn(name="group_id", referencedColumnName="id")},
@@ -34,46 +39,72 @@ class Group
 	 * @ORM\Column(type="text")
 	 */
 	private $description;
+
+	// ID, getter.
+	public function getId()
+	{
+		return $this->id;
+	}
 	
+	// Group-specific roles, getter.
 	public function getRoles()
 	{
 		return $this->roles;
 	}
-	
+
+	// Adds group-specific role, pseudo-setter.
 	public function addRole($role)
 	{
 		array_push($this->roles, $role);
 	}
-	
+
+	// Removes group-specific role, pseudo-setter.
 	public function removeRole($role)
 	{
 		if(($key = array_search($role, $this->roles)) !== false) {
 			unset($this->roles[$key]);
 		}
 	}
-	
+
+	// Group-specific permissions, getter.
 	public function getPermissions()
 	{
 		return $this->permissions;
 	}
-	
+
+	// Adds group-specific permission, pseudo-setter.
 	public function addPermission($permission)
 	{
 		array_push($this->permissions, $permission);
 	}
-	
+
+	// Removes group-specific permission, pseudo-setter.
 	public function removePermission($permission)
 	{
 		if(($key = array_search($permission, $this->permissions)) !== false) {
 			unset($this->permissions[$key]);
 		}
 	}
+
+	// Name, getter.
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	// Name, setter.
+	public function setName($name)
+	{
+		$this->name = $name;
+	}
 	
+	// Description, getter.
 	public function getDescription()
 	{
 		return $this->description;
 	}
-	
+
+	// Description, setter.
 	public function setDescription($description)
 	{
 		$this->description = $description;
